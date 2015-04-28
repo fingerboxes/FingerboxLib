320182fee833d8c59693ff864c1ab78dfa3a7db2
fingerboxes <fingerboxes>
2015-04-28 00:45:41 -0700

    I can never be happy.

--- Source/Extensions/CoreExtensions.cs
+++ Source/Extensions/CoreExtensions.cs
@@ -7,11 +7,11 @@ namespace FingerboxLib
 {
     public static class CoreExtensions
     {
-        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        public static T Clamp<T>(this T value, T min, T max) where T : IComparable<T>
         {
-            if (val.CompareTo(min) < 0) return min;
-            else if (val.CompareTo(max) > 0) return max;
-            else return val;
+            if (value.CompareTo(min) < 0) return min;
+            if (value.CompareTo(max) > 0) return max;
+            return value;
         }
 
         public static IEnumerable<T> TakePercent<T>(this ICollection<T> source, double percent)
